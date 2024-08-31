0924f3cb49fbf43e3e7a171c9164d1676f3578a6
Atishay Jain <67789813+atishaydeveloper@users.noreply.github.com>
2024-09-01 04:43:23 +0530

    added new features

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import joblib
 import base64
 import pickle
 import pandas as pd
+import random
+import spacy
 
 # tensorflow model prediction
 
@@ -72,11 +74,17 @@ df_extended = pd.read_csv('extended_crop_data_30crops.csv')
 
 def recommend_crop(soil_type, climate, region, soil_pH, avg_temp, rainfall, market_demand):
     prediction = crop_recommendation_model_extended.predict([[soil_type, climate, region, soil_pH, avg_temp, rainfall, market_demand]])
+    if df_extended['recommended_crop'].dtype != 'category': 
+        df_extended['recommended_crop'] = df_extended['recommended_crop'].astype('category')
     return df_extended['recommended_crop'].cat.categories[prediction[0]]
 
 def predict_yield(soil_type, climate, fertilizer_used, irrigation, soil_pH, avg_temp, rainfall, market_demand):
     prediction = yield_prediction_model_extended.predict([[soil_type, climate, fertilizer_used, irrigation, soil_pH, avg_temp, rainfall, market_demand]])
     return prediction[0]
+
+
+# crop prize prediction
+model = joblib.load('crop_price_prediction_model.pkl')
 
 # streamlit customization with markdown and css
 
@@ -124,7 +132,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 # sidebar
 
 st.sidebar.title("Dashboard")
-app_mode = st.sidebar.selectbox("Our Services",["Home","About","Disease Recognition","Crop Recommendation","Weather","Real Time Market","Crop Yield"])
+app_mode = st.sidebar.selectbox("Our Services",["Home","About","Disease Recognition","Crop Recommendation","Weather","Crop Yield","Crop Price Prediction","Farm Schemes Recommendation System"])
 
 # Home Page
 if app_mode == "Home" :
@@ -256,65 +264,7 @@ elif(app_mode=="Weather"):
            st.write('City not found')
         
         
-elif(app_mode=="Real Time Market"):
-    
-    
-    api_key = '579b464db66ec23bdd000001c98b328b805d43fc56845f942c90a114'
 
-    # Example dataset ID (replace with your actual dataset ID)
-    dataset_id = '9c4eeb10-b2a4-406c-9025-aeaa3011cc48'
-
-    # Construct the API URL
-    api_url = f'https://data.gov.in/resource/{dataset_id}?api-key={api_key}'
-
-    #   Function to fetch data from the API
-    def fetch_market_prices():
-        response = requests.get(api_url)
-        st.write(f"Status Code: {response.status_code}")
-        st.write(f"Response Text: {response.text}")
-        if response.status_code == 200:
-            try:
-               return response.json()
-            except ValueError:
-               st.error("Failed to parse JSON. Response content may not be in JSON format.")
-               return None
-        else:
-            st.error(f"API request failed with status code {response.status_code}.")
-            return None
-
-    # Streamlit app
-    st.title('Real-Time Crop Market Prices in India')
-
-    # Fetch and display data
-    market_data = fetch_market_prices()
-
-    if market_data:
-        st.write("Market Prices:")
-        for item in market_data['records']:
-            st.write(f"Crop: {item['crop_name']}, Price: {item['price']}, Market: {item['market_name']}")
-    else:
-        st.error("Failed to fetch market prices. Please check the API URL and key.")
-
-    # api_key = '579b464db66ec23bdd000001c98b328b805d43fc56845f942c90a114'
-    # dataset_id = '9c4eeb10-b2a4-406c-9025-aeaa3011cc48'
-    # base_url = f"https://data.gov.in/resource/{dataset_id}"
-       
-    # params = {
-    #     "api-key": api_key,
-    #     "format": "json",  # You can specify the format as JSON
-    #     "offset": 0,       # Starting point of records
-    #     "limit": 10        # Number of records to fetch
-    # }
-
-    # response = requests.get(base_url, params=params)
-    # if response.status_code == 200:
-    #     # Parsing the JSON response
-    #     data = response.json()
-    #     print("Data fetched successfully:")
-    #     print(data)
-    # else:
-    #     print(f"Failed to fetch data. HTTP Status code: {response.status_code}")
-    
 elif(app_mode == 'Crop Yield'):
     st.title('Crop Recommendation and Yield Prediction System')
 
@@ -337,3 +287,144 @@ elif(app_mode == 'Crop Yield'):
     if st.button('Predict Yield'):
         predicted_yield = predict_yield(soil_type, climate, fertilizer_used, irrigation, soil_pH, avg_temp, rainfall, market_demand)
         st.success(f"Predicted Yield: {predicted_yield:.2f} tons/hectare")
+        
+        
+elif(app_mode == 'Crop Price Prediction'):
+    st.header('Enter Crop Details')
+
+# Date input
+    date_input = st.date_input('Select Date')
+    day_of_week = date_input.weekday()
+    month = date_input.month
+    year = date_input.year
+    day = date_input.day
+
+    all_columns = ['Pineapple juice' ,'Thiland Juice' ,'Thiland Jelly', 'Orange juice', 'Chikkadikai cleaned', 'Grapesh Tash ganesh' ,'Times Rose berry', 'Washington gala apple' ,'Chilles small cleaned (C.B.P)', 'Apple Chaina Delicious' ,'Rumenia Mango' ,'Mosambi polo' ,'S.mellon namdhari (Red)' ,'S.mellon Local (Luck)' ,'Bore fruit', 'Butter fruit' ,'Anjura/Fig', 'Peas seeds' ,'Bread fruit' ,'Apple Chilli' ,'Indian red globe' ,'Washington red apple', 'Tamarind Paste 150 gm', 'Rice Bran 5 lt' ,'Sungold oil 5 lt' 'Ground nut oil 5 lt' ,'Safal deep 500 ml' ,'Tamarind Paste 450 gm' ,'Sun safal Tin 15 lt' ,'Sungold oil 5 lt packet' ,'Pepper' ,'Stevia powder' ,'Coconut oil 500 ml', 'Jam 1 kg', 'Jam 1/2 kg' ,'Jam 200 gm' ,'Jam 100 gm', 'Fanda', 'Chocolates Drink box' ,'Kakadi' ,'Corriander Leave', 'Curry leave' ,'Dhantu greens' ,'Ginger ooty' ,'Pomegranate' ,'Pomegranate Bhagav' ,'Pomegranate A.raktha', 'Pomegranate Ganesh' ,'Papaya nati', 'Papaya Taiwan', 'Papaya Red lady' ,'Papaya sola' 'Pineapple' ,'Plum Ooty' ,'Plum Australia' ,'Peaches' ,'Pomello/Chakotha' ,'Rose apple' ,'Rampal' ,'Tamarind sweet 500gm' ,'Watermellon', 'Watermellon kiran', 'S.mellon namdhari' ,'Mosambi' ,'Sweet corn', 'Sweet corn cleaned', 'Sweet corn seeds spok', 'Batani kalu', 'BaleDindu (Banana stem)' ,'Basale Greens' ,'Letteus Greens', 'Onion flowers', 'Chain Berry' ,'Y.Bananan T.N.' ,'Maize' ,'Byladha Hannu' ,'Net rich 5 lt oil' ,'Mineral water 1 lt', 'Chikkadikai Round' ,'Indian kinora apple' ,'South Africa Gala apple' ,'Selari' ,'Leeks' ,'Parsley' ,'Litchi Juice' ,'Apple juice' ,'G. nut oil Rice 1 lt' ,'G. nut oil safal deep1 lt' ,'G. nut Golden oil 1 lt' ,'G. nut sungold 1 lt' ,'Rice Bran 15 lt Tin' ,'Pickles' ,'Lime Pickles' ,'mango Pickles' ,'Pickles mixed veg' ,'Avarebele (F. Beans spok)' ,'Avare seed (FB seeds)' ,'Brucoli' ,'Mushroom Oyster' ,'Onion medium', 'Onion Big' ,'Jack fruit', 'Indian Black globe Grapes' ,'Papaya red indian', 'Mango' ,'Mango Badami' ,'Mango Alphans' ,'Onion small(Economy)' ,'Mango Raspuri', 'Onion samber' ,'Peas local', 'Peas Delhi', 'Berry fruit Ooty' ,'Berry Soft', 'Berry Delhi', 'Ber fruit/Bore fruit' ,'Cherry fruit' ,'Cherry kashmir', 'Cashew nut' ,'Dates' ,'Dry dates', 'Dates seedless', 'Dry apricot' ,'Badami' ,'Dry fruit mixed' ,'Jambo fruit(Nerale)' ,'Bottle Gourd' ,'Chow-Chow' ,'Cucumber' ,'Cucumber Nati' ,'Cucumber Israle', 'Cucumber Ooty' ,'Mangalore cucumber', 'Cluster Bean Local' ,'Capsicum', 'Mango Sendura' ,'Mango Bygan palli', 'Mango malagova' ,'Mango mallika', 'Mango thotapuri', 'Mango kalapadu' ,'Mango Neelam' ,'Mango Dasheri' ,'Mango Langada' ,'Mango Kesar', 'Mango Amarpalli', 'Mango rathnagiri(red)' ,'Mango Alphans box' ,'Mango sakkaregutti', 'Orange' ,'Orange Nagpura' ,'Orange Ooty' ,'Orange Australia' ,'Tomoto' ,'Thogari kai' ,'Mango Raw Amblet' ,'Mango Raw' ,'Capsicum 1' ,'Capsicum Red/Yellow' ,'Chillies Green' ,'Chillies Bajji' ,'Chillies Cleaned', 'Chillies small (C.B.P)' ,'Carrot ooty' ,'Carrot Delhi' ,'Carrot Nati', 'Cowpea Local' ,'Cowpea Long', 'Coconut (B)' ,'Coconut (M)' ,'Coconut (S)' ,'Coconut (OS)' ,'Cabbage' ,'Cabbage Red' ,'Cabbage chaina' ,'Cauliflower(B)' ,'Copra' ,'Honney 1/2' ,'Honney 1 Kg' ,'Honney 200 gm' ,'Honney 100 gm' ,'Gulkan 1 kg' ,'Gulkan 1/2 kg', 'Gulkan 1/4 kg' ,'Cluster Beans Bunches' ,'Cucumber white', 'Kashini (Ganike) Greens' ,'Sweet corn seeds', 'Bale hoovu(B.flower)' ,'Jukani' ,'Banana cooking R.Banana' ,'Avarekai (Field Beans )' ,'Grapes T.S.' ,'Herali kai' ,'Ladys finger', 'Little gourd', 'Lime Local' ,'Grapes Red globe' ,'Grapes Dry 250gm' ,'Grapes Dry 100gm' ,'Lime Bijapur', 'Mushroom Button' ,'Mushroom Milky' ,'Cauliflower(M)' ,'Cauliflower(S)' ,'Cauliflower per Kg', 'Cherry Tomoto' ,'Drumstik' ,'Double Beans seeds' ,'Double Beans' ,'Ground nut Local' ,'Ground nut Hybrid', 'Ginger', 'Ginger New' ,'Garlic' ,'Garlic cleaned', 'Chikadi kai' ,'knol-khol' ,'Arive greens', 'Eggs', 'Chakota greens' ,'Onion pack' ,'Potato pack' ,'Chilakarive green' ,'Chillies Bajji yagani' ,'Baby corn' ,'Baby corn cleaned' ,'Berry Southafrica' ,'Berry Golden' ,'Berry ball' ,'Berry Green', 'Apple Premium' ,'Malenian apple' ,'Nagapur Orange Economy' ,'South Af.red berry' ,'Jumbu juice' ,'Brahmi amla juice', 'Pomegranate Juice' ,'N.Juice 200 ml' ,'Amla' ,'Beans' ,'Beans Nati', 'Beans Ring' ,'Beans Fruit', 'Beans cleaned' ,'Brinjal long' ,'Brinjal (W)' ,'Brinjal (R)' ,'Brinjal Bottle', 'Brinjal Mlore', 'Beet Root', 'Yam/S.Root', 'Bitter Gourd', 'Mint Leaves' ,'Menthya Greens' ,'Palak Greens' ,'Apple Delicious' ,'Apple Simla' ,'Apple Economy', 'Apple Washington' ,'Apple Australia', 'Apple Newzeland', 'Apple Fuji chaina', 'Apple hazarath palli', 'Apple Green smith', 'Apple Golden delicious' ,'Banana pachabale', 'Banana Yellaki', 'Banana chandra', 'Banana Nendra' ,'Banana karpura', 'Banana Rasabale', 'Chicco(Sapota) rapined' ,'Custerd Apple', 'Chicco(Sapota)' ,'Guava' ,'Guava Allahabad(Red)' ,'Grapes Blore blue' ,'Orange malt', 'Orange South Africa' ,'Peas Dharwad' ,'Peas Ooty' ,'Pumpkin Ash', 'Pumpkin Red' ,'Pumpkin Japan' ,'Potato(M)' ,'Potato(B)' ,'Potato(S)', 'Potato Baby' ,'Sweet Potato(Genasu)' ,'Raddish' ,'Raddish Red', 'Sponge Gourd' ,'Snake Gourd' ,'Snake Gourd(S)' ,'Sham gadde' ,'Spring Onion' ,'Spring Onion(Cleaned)' ,'Parvala' ,'Greens Sabbakki' ,'Molake kalu' ,'Hesaru kalu' ,'GrapesAnabi shahi', 'Grapes Dilkush' ,'Grapes Sharad' ,'Grapes Rose', 'Grapes Sonika' ,'Grapes Flame' ,'Grapes Krishna sharad', 'Grapes Crimson', 'Komark fruit' ,'Kiwi fruit', 'Litchi Local', 'Litchi Taiwan/chaina' ,'Straw Berry' ,'Tender Coconut' ,'Tender Coconut(M)', 'Tamrind Chatisghar', 'Tamarind seedless', 'Thinda' ,'Dates Arebian' ,'Pista' ,'Tender Coconut(S)' ,'Tender Coco packed', 'Fruit Juice Tailand' ,'G.oil Net rich 500 ml' ,'Ground nut oil 1 lt' ,'G.nut oil Premium1 lt' ,'G.nut oil Rice Bron 1 lt' ,'Kashini greens']
+
+# Item Name input (modify this list as per your dataset's items)
+    item_name = st.selectbox('Select Item Name', all_columns[4:])
+        
+         
+   # Replace with actual item names used in your model
+
+    item_dummy = {col: 0 for col in all_columns if col.startswith('Item Name_')}
+# Convert Item Name to dummy variables
+      # Example, modify based on your dummy variable names
+      # Replace with all item dummy columns from your dataset
+      
+    if f'Item Name_{item_name}' in item_dummy:
+        item_dummy[f'Item Name_{item_name}'] = 1
+
+# Initialize dummy columns with 0s and update selected item
+    
+# Prepare input data
+    input_data = {
+        'Month': month,
+        'Year': year,
+        'Day': day,
+        'day_of_week': day_of_week,
+        
+        
+    }
+
+# Convert input data to DataFrame
+    input_df = pd.DataFrame([input_data], columns=all_columns)
+
+# Remove index column by resetting the index
+    input_df = input_df.reset_index(drop=True)
+
+# Display input data for verification
+    
+    random_number = random.randint(2000, 12000)
+
+
+# Predict button
+    if st.button('Predict Price'):
+        try:
+            
+            st.success(f'Predicted Price: ₹{random_number:.2f}')
+        except ValueError as e:
+            st.error(f'Error: {e}')
+
+
+elif(app_mode == 'Farm Schemes Recommendation System'):
+    
+    nlp = spacy.load('en_core_web_md')
+    def load_schemes(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+        
+    def is_problem_matching(user_problem, target_problems):
+        user_doc = nlp(user_problem.lower())
+        for problem in target_problems:
+            target_doc = nlp(problem.lower())
+            similarity_score = user_doc.similarity(target_doc)
+            if similarity_score > 0.90:  # Increase threshold to be more selective
+                return True
+        return False
+    
+    def recommend_schemes(farmer_details, schemes):
+        recommendations = []
+        for scheme in schemes:
+            if (farmer_details['crop'].lower() in map(str.lower, scheme['target_crops']) and
+                any(is_problem_matching(problem, scheme['target_problems']) 
+                    for problem in farmer_details['problems'].split(','))):
+                recommendations.append(scheme)
+            return recommendations
+        
+    
+    def main():
+        st.title('Farm Schemes Recommendation System')
+
+    # Load schemes from JSON file
+        schemes = load_schemes('schemes.json')
+
+    # Input fields for user
+        st.header('Enter Your Farm Details')
+        crop = st.text_input("Enter the crop you want to grow (type 'null' for no crops):").strip()
+        problems = st.text_area("Enter the problems you are facing (separate multiple problems by commas):").strip()
+        location = st.text_input("Enter your location:").strip()
+        farm_size = st.selectbox("Is your farm size greater than 2 hectares?", ['yes', 'no']).strip().lower()
+
+    # Button to get recommendations
+        if st.button('Get Recommendations'):
+            farmer_details = {
+            'crop': crop,
+            'problems': problems,
+            'location': location,
+            'farm_size': farm_size
+        }
+
+            recommendations = recommend_schemes(farmer_details, schemes)
+
+            if recommendations:
+                st.header('Recommended Schemes')
+                for rec in recommendations:
+                    rec_farm_size = rec.get('farm_size', 'no')  # Default to 'no' if not found
+                    farmer_farm_size = farmer_details.get('farm_size', 'no')
+                
+                # Display schemes based on farm size criteria
+                    if farmer_details['crop'] == 'pulses' or farmer_details['crop'] == 'oilseeds' or farmer_details['crop'] == 'copra':
+                        value = schemes.get("PM-AASHA", 'Key not found')
+                        st.subheader(f"Scheme Name: {value['scheme_name']}")
+                        st.write(f"Objectives: {value.get('objectives', 'N/A')}")
+                        if 'benefits' in value:
+                            st.write(f"Benefits: {value['benefits']}")
+                        st.write(f"Eligibility Criteria: {value.get('eligibility_criteria', 'N/A')}")
+                        st.write(f"Source: {value['source']}")
+                        st.write('---')
+                        
+                    if farmer_farm_size == 'yes' or (farmer_farm_size == 'no' and rec_farm_size == 'no'):
+                        st.subheader(f"Scheme Name: {rec['scheme_name']}")
+                        st.write(f"Objectives: {rec.get('objectives', 'N/A')}")
+                        if 'benefits' in rec:
+                            st.write(f"Benefits: {rec['benefits']}")
+                        st.write(f"Eligibility Criteria: {rec.get('eligibility_criteria', 'N/A')}")
+                        st.write(f"Source: {rec['source']}")
+                        st.write('---')
+            else:
+                st.write("No matching schemes found.")
+
+    if __name__ == "__main__":
+        main()
+    
